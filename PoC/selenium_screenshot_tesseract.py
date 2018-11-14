eed72dfeca4380fe169572d16c39ac6a3a27b330
Mark Schneemann <git@f5w.de>
2018-11-14 12:38:43 +0100

    Additional Tesseract Parameters - still no results

--- PoC/selenium_screenshot_tesseract.py
+++ PoC/selenium_screenshot_tesseract.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 try:
     from PIL import Image
 except ImportError:
@@ -16,7 +17,7 @@ driver = webdriver.Chrome(
 
 # set parameters
 x = 1920
-y = 4000
+y = 1080
 target = "https://www.uni-giessen.de/ueber-uns/datenschutz"
 
 # lets rock!
@@ -24,10 +25,12 @@ print("Resizing window to: " + str(x) + "x" + str(y))
 driver.set_window_size(x, y)
 print("Loading website: " + target)
 driver.get(target)
+print("Building a custom wordlist (based on the html content)")
 print("Taking a screenshot")
 driver.save_screenshot('screenshot.png')
 print("Screenshot -> Tesseract -> Text")
-text = pytesseract.image_to_string(Image.open('screenshot.png'))
+config = ('--psm 11 --oem 3 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@-.')
+text = pytesseract.image_to_string(Image.open('screenshot.png'), config=config)
 print("If @ in token -> print!")
 for line in text.split("\n"):
     for token in line.split(" "):
